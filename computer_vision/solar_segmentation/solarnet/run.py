b31abe4380cabffb96060c211c807d979ff37605
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-04-07 11:00:25 -0400

    Allow the data folder to be defined from the run class

--- computer_vision/solar_segmentation/solarnet/run.py
+++ computer_vision/solar_segmentation/solarnet/run.py
@@ -13,34 +13,39 @@ from solarnet.models import Classifier, Segmenter, train_classifier, train_segme
 class RunTask:
 
     @staticmethod
-    def make_masks():
-        mask_maker = MaskMaker()
+    def make_masks(data_folder='data'):
+        mask_maker = MaskMaker(data_folder=Path(data_folder))
         mask_maker.process()
 
     @staticmethod
-    def split_images():
-        splitter = ImageSplitter()
+    def split_images(data_folder='data'):
+        splitter = ImageSplitter(data_folder=Path(data_folder))
         splitter.process()
 
     @staticmethod
-    def train_classifier(max_epochs=5, val_size=0.1, test_size=0.1,
+    def train_classifier(max_epochs=5, val_size=0.1, test_size=0.1, data_folder='data',
                          device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')):
+        data_folder = Path(data_folder)
 
         model = Classifier()
         if device.type != 'cpu': model = model.cuda()
-        dataset = ClassifierDataset()
+
+        processed_folder = data_folder / 'processed'
+        dataset = ClassifierDataset(processed_folder=processed_folder)
 
         # make a train and val set
         train_mask, val_mask, test_mask = make_masks(len(dataset), val_size, test_size)
 
         dataset.add_mask(train_mask)
         train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-        val_dataloader = DataLoader(ClassifierDataset(mask=val_mask), batch_size=64, shuffle=True)
-        test_dataloader = DataLoader(ClassifierDataset(mask=test_mask), batch_size=64)
+        val_dataloader = DataLoader(ClassifierDataset(mask=val_mask, processed_folder=processed_folder),
+                                    batch_size=64, shuffle=True)
+        test_dataloader = DataLoader(ClassifierDataset(mask=test_mask, processed_folder=processed_folder),
+                                     batch_size=64)
 
         train_classifier(model, train_dataloader, val_dataloader, max_epochs=max_epochs)
 
-        savedir = Path('data/models')
+        savedir = data_folder / 'models'
         if not savedir.exists(): savedir.mkdir()
         torch.save(model.state_dict(), savedir / 'classifier.model')
 
@@ -57,23 +62,27 @@ class RunTask:
         np.save(savedir / 'classifier_true.npy', np.concatenate(true))
 
     @staticmethod
-    def train_segmenter(max_epochs=1, val_size=0.1, test_size=0.1, use_classifier=True,
+    def train_segmenter(max_epochs=1, val_size=0.1, test_size=0.1,
+                        data_folder='data', use_classifier=True,
                         device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')):
+        data_folder = Path(data_folder)
         model = Segmenter()
         if device.type != 'cpu': model = model.cuda()
 
-        model_dir = Path('data/models')
+        model_dir = data_folder / 'models'
         if use_classifier:
             classifier_sd = torch.load(model_dir / 'classifier.model')
             model.load_base(classifier_sd)
-
-        dataset = SegmenterDataset()
+        processed_folder = data_folder / 'processed'
+        dataset = SegmenterDataset(processed_folder=processed_folder)
         train_mask, val_mask, test_mask = make_masks(len(dataset), val_size, test_size)
 
         dataset.add_mask(train_mask)
         train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-        val_dataloader = DataLoader(SegmenterDataset(mask=val_mask), batch_size=64, shuffle=True)
-        test_dataloader = DataLoader(SegmenterDataset(mask=test_mask), batch_size=64)
+        val_dataloader = DataLoader(SegmenterDataset(mask=val_mask, processed_folder=processed_folder),
+                                    batch_size=64, shuffle=True)
+        test_dataloader = DataLoader(SegmenterDataset(mask=test_mask, processed_folder=processed_folder),
+                                     batch_size=64)
 
         train_segmenter(model, train_dataloader, val_dataloader, max_epochs=max_epochs)
 
