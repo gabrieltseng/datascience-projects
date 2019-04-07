c58a4c9389aa55ed006dedc57d3d67caed05a197
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2019-04-07 09:31:07 -0400

    Add test set when training the classifier

--- computer_vision/solar_segmentation/solarnet/run.py
+++ computer_vision/solar_segmentation/solarnet/run.py
@@ -3,6 +3,7 @@ from torch.utils.data import DataLoader
 
 import numpy as np
 from pathlib import Path
+from tqdm import tqdm
 
 from solarnet.preprocessing import MaskMaker, ImageSplitter
 from solarnet.datasets import ClassifierDataset
@@ -22,7 +23,7 @@ class RunTask:
         splitter.process()
 
     @staticmethod
-    def train_classifier(max_epochs=5, val_set=0.1,
+    def train_classifier(max_epochs=5, val_set=0.1, test_set=0.1,
                          device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')):
 
         model = Classifier()
@@ -30,17 +31,34 @@ class RunTask:
         dataset = ClassifierDataset()
 
         # make a train and val set
-        train_mask = np.random.rand(len(dataset)) < (1 - val_set)
-        val_mask = ~train_mask
+        mask = np.random.rand(len(dataset))
+        train_mask = mask < (1 - (val_set + test_set))
+        val_mask = (mask > (1 - (val_set + test_set))) & (mask < 1 - test_set)
+        test_mask = mask < (1 - test_set)
 
         dataset.add_mask(train_mask)
         val_dataset = ClassifierDataset(mask=val_mask)
+        test_dataset = ClassifierDataset(mask=test_mask)
 
         train_dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
         val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=True)
+        test_dataloader = DataLoader(test_dataset, batch_size=64)
 
         train_classifier(model, train_dataloader, val_dataloader, max_epochs=max_epochs)
 
         savedir = Path('data/models')
         if not savedir.exists(): savedir.mkdir()
         torch.save(model.state_dict(), savedir / 'classifier.model')
+
+        # save predictions for analysis
+        print("Generating test results")
+        preds, true = [], []
+        with torch.no_grad():
+            for test_x, test_y in tqdm(test_dataloader):
+                test_preds = model(test_x)
+                preds.append(test_preds.squeeze(1).cpu().numpy())
+                true.append(test_y.cpu().numpy())
+
+        np.save(savedir / 'classifier_preds.npy', np.concatenate(preds))
+        np.save(savedir / 'classifier_true.npy', np.concatenate(true))
+
