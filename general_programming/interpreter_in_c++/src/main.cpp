abeb600e744fded424a1a4b648e3cd499937e842
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2020-03-03 22:27:30 +0800

    Remove commented line

--- general_programming/interpreter_in_c++/src/main.cpp
+++ general_programming/interpreter_in_c++/src/main.cpp
@@ -7,6 +7,5 @@ int main() {
     std::cout << "Hello! This is the Monkey programming language \n";
     std::cout << "Feel free to type in commands. \n";
 
-    // std::istream input_stream = std::stdin;
     repl::Start(std::cin, std::cout);
 }
